f58275eeb9c7c6245771f5e896145c29d8c98935
Kunal Patel <kunal_patel@symantec.com>
2016-09-21 14:00:37 +0530

    add another controller and jackson fixes

--- src/main/java/org/coderearth/kitchens/appstarter/AppConfig.java
+++ src/main/java/org/coderearth/kitchens/appstarter/AppConfig.java
@@ -1,7 +1,12 @@
 package org.coderearth.kitchens.appstarter;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 /**
  * Created by kunal_patel on 9/21/16.
@@ -9,4 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "org.coderearth.kitchens.appstarter")
 public class AppConfig {
+
+    @Bean
+    public View jsonTemplate() {
+        MappingJackson2JsonView view = new MappingJackson2JsonView();
+        view.setPrettyPrint(true);
+        return view;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        return new BeanNameViewResolver();
+    }
+
 }
--- src/main/java/org/coderearth/kitchens/appstarter/HelloController.java
+++ src/main/java/org/coderearth/kitchens/appstarter/HelloController.java
@@ -1,13 +1,15 @@
 package org.coderearth.kitchens.appstarter;
 
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 
 /**
  * Created by kunal_patel on 9/21/16.
@@ -17,9 +19,14 @@ public class HelloController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping("/")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("hello world !!");
+    }
+
+    @RequestMapping(value = "/hi", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> sayJsonHello() {
+        return ResponseEntity.ok(ImmutableMap.of("message", "hello world !!"));
     }
 
     @PostConstruct
